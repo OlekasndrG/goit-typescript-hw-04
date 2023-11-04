3d6fc288a66806a9ba679697511bef7e4d70fd7d
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-11-04 11:44:13 +0200

    init

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,7 +3,10 @@ import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
-
+type SelectedMenu = { id?: MenuIds };
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
 // Додати тип Menu Selected
 
 const MenuSelectedContext = createContext<MenuSelected>({
@@ -11,13 +14,15 @@ const MenuSelectedContext = createContext<MenuSelected>({
 });
 
 // Додайте тип MenuAction
-
+type MenuAction = {
+  onSelectedMenu: (arg: SelectedMenu) => void;
+};
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +53,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
@@ -59,7 +64,7 @@ function MenuComponent({ menus }: PropsMenu) {
     <>
       {menus.map((menu) => (
         <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
+          {menu.title}
           {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
         </div>
       ))}
